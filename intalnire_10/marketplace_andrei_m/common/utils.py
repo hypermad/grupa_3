c691d9d3e9b740000f73dd7106ff943077f06f66
hypermad <muresan.andrei.pln@gmail.com>
2022-04-11 22:04:15 +0300

    update intalnirea 10 1

--- intalnire_10/marketplace_andrei_m/common/utils.py
+++ intalnire_10/marketplace_andrei_m/common/utils.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
 from baza_de_date.functii import citeste_datele_din_baza_de_date, scrie_datele_in_baza_de_date
-
+from flask import Response
 
 def genereaza_id(detalii_comanda):
     hash_object = hashlib.md5(bytes(json.dumps(detalii_comanda), encoding='UTF-8'))
@@ -15,5 +15,7 @@ def sterge(identificator_de_sters, key):
         datele[key].pop(identificator_de_sters)
         scrie_datele_in_baza_de_date(datele)
         return 200, f"User: {identificator_de_sters} has been successfully deleted"
+        # return Response(status=200, response=f"User: {identificator_de_sters} has been successfully deleted") # a doua varianta
     else:
         return 404, f"User: {identificator_de_sters} does not exist in the database"
+        # return Response(status=404, response=f"User: {identificator_de_sters} does not exist in the database") # a doua varianta
